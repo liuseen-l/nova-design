e2693a7d477b0d2ac65a794b55e47bcb574dbfff
liuseen-l <773890440@qq.com>
2023-08-18 23:05:26 +0800

    feat: init

---
+++ packages/utils/bem.ts
@@ -0,0 +1,79 @@
+function isObject(val: unknown): val is object {
+  return val !== null && typeof val === 'object'
+}
+
+function isArray(val: unknown): val is string[] {
+  return Array.isArray(val)
+}
+
+function isString(val: unknown): val is string {
+  return typeof val === 'string'
+}
+
+type BEMElement = string
+type BEMModifier =
+  | (string | undefined | false)[]
+  | Record<string, boolean | string | undefined>
+
+function createModifier(prefixClass: string, modifierObject?: BEMModifier) {
+  let modifiers: string[] = []
+  if (isArray(modifierObject)) {
+    modifiers = modifierObject
+      .map((modifier) => {
+        if (!modifier)
+          return ''
+        return `${prefixClass}--${modifier}`
+      })
+      .filter(Boolean)
+  }
+  else if (isObject(modifierObject)) {
+    modifiers = Object.entries(modifierObject).map(([modifier, value]) => {
+      if (!value)
+        return ''
+      return `${prefixClass}--${modifier}`
+    })
+  }
+  return modifiers
+}
+
+/**
+ * CSS BEM
+ * @example
+ * const bem = createCssScope('button')
+ * bem() // button
+ * bem('label') // button__label
+ * bem({ disabled }) // button button--disabled
+ * bem('label', { disabled }) // button__label button__label--disabled
+ * bem(['disabled', 'primary']) // button button--disabled button--primary
+ * bem(['disabled', 'primary']) // button button--disabled button--primary
+ * bem([type, status, shape, size], {loading: loading,long: long,disabled: disabled}),
+ * bem('main',[type, status, shape, size], {loading: loading,long: long,disabled: disabled}),
+ */
+
+export function createCssScope(prefix: string, identity = 'yk') {
+  const prefixClass = `${identity}-${prefix.replace(identity, '')}`
+
+  return (
+    elementOrModifier?: BEMElement | BEMModifier,
+    modifier?: BEMModifier,
+    modifierLater?: BEMModifier,
+  ) => {
+    if (!elementOrModifier)
+      return prefixClass
+    if (isString(elementOrModifier)) {
+      const element = `${prefixClass}__${elementOrModifier}`
+      if (!modifier)
+        return element
+      return [
+        element,
+        ...createModifier(element, modifier),
+        ...createModifier(element, modifierLater),
+      ]
+    }
+    return [
+      prefixClass,
+      ...createModifier(prefixClass, elementOrModifier),
+      ...createModifier(prefixClass, modifier),
+    ]
+  }
+}
